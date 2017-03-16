ce9b5368d04d47e9589bd247c49374d85f0e20d5
Min RK <benjaminrk@gmail.com>
2017-03-16 10:46:35 +0100

    0.2.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 setup_args = dict(
     name                = 'condamagic',
     py_modules          = ['condamagic'],
-    version             = '0.1.0',
+    version             = '0.2.0',
     description         = "%condamagic for IPython",
     long_description    = " ",
     author              = "Min RK",
